4cec0e585b862a53bd268bd4b92d99bf0ac47e9a
Ivan Lupolov <iv.lupolov@gmail.com>
2020-04-09 15:24:00 +0300

    subdivision is working

--- src/main/java/gun/service/repository/BattleManagerRepository.java
+++ src/main/java/gun/service/repository/BattleManagerRepository.java
@@ -3,7 +3,6 @@ package gun.service.repository;
 import gun.service.dto.UnitDamageDto;
 import gun.service.dto.UnitDto;
 import gun.service.service.gun.systems.Battlefield;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
@@ -12,20 +11,21 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Repository
-@RequiredArgsConstructor
 public class BattleManagerRepository {
 
-    @Value("${battle-manager-server.host}")
-    private String host;
-    @Value("${battle-manager-server.port}")
-    private Integer port;
-
-    private String template = "http://"+ host +":"+ port +"/units";
+    private final String template;
     private final RestTemplate restTemplate;
+
+    public BattleManagerRepository(@Value("${battle-service-server.host}") String host,
+                                   @Value("${battle-service-server.port}") Integer port,
+                                   RestTemplate restTemplate) {
+        this.template = "http://"+ host +":"+ port;
+        this.restTemplate = restTemplate;
+    }
 
     public UnitDto findUnitByCoordinateAndBattleId(Integer posX, Integer posY, Integer battleId) {
 
-        String url = template +"/x/"+ posX +"/y/"+ posY +"/battle/"+ battleId;
+        String url = template +"/units/x/"+ posX +"/y/"+ posY +"/battle/"+ battleId;
 
         try {
             ResponseEntity<UnitDto> response = restTemplate.getForEntity(url, UnitDto.class);
@@ -37,7 +37,7 @@ public class BattleManagerRepository {
 
     public void setUnitDamage(UnitDamageDto damageDto, Integer battleId) {
 
-        String url = template +"/battle/"+ battleId +"/units/damage";
+        String url = template +"/units/damage/battle/"+ battleId;
         HttpEntity<UnitDamageDto> request = new HttpEntity<>(damageDto);
 
         restTemplate.patchForObject(url,request, Void.class);
